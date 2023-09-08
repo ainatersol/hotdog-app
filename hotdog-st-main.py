1277e3d637112bd009c2003749f22fbb3687284f
ainatersol <aina.tersol@gmail.com>
2023-09-08 18:12:25 +0200

    loader

--- hotdog-st-main.py
+++ hotdog-st-main.py
@@ -4,12 +4,6 @@ import numpy as np
 
 from fastai.vision.all import *
 
-learn = load_learner('hotdog.pkl')
-categories = ('hotdog', 'pasta', 'pizza', 'salad', 'sandwich')
-
-def classify_images(img):
-    pred, idx, probs = learn.predict(img)
-    return dict(zip(categories, map(float, probs)))
 
 st.write(""" 
          # Simple streamlit app for hotdog classification
@@ -18,4 +12,22 @@ st.write("""
 
 st.sidebar.header('User image upload')
 
-def user_input 
+def user_input(): 
+    images = st.sidebar.file_uploader('load images to classify', 
+                             type=['png', 'jpg'],
+                            accept_multiple_files=True )
+    
+    return images 
+
+uploaded_files = user_input()
+for uploaded_img in uploaded_files:
+    bytes_data = uploaded_img.read()
+    st.write("filename:", uploaded_img.name)
+    st.write(bytes_data)
+
+learn = load_learner('hotdog.pkl')
+categories = ('hotdog', 'pasta', 'pizza', 'salad', 'sandwich')
+
+def classify_images(img):
+    pred, idx, probs = learn.predict(img)
+    return dict(zip(categories, map(float, probs)))
